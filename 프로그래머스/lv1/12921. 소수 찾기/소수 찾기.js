3ff1878ac7cd6704d20520cddcc4bd7a316366f7
Seungmin Lim <92246102+forest-6@users.noreply.github.com>
2023-01-02 15:59:10 +0900

    [level 1] Title: 소수 찾기, Time: 26.40 ms, Memory: 45.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,16 +1,11 @@
-function solution(n) {
-    const s = new Set();
-    for(let i=1; i<=n; i+=2){
-        s.add(i);
-    }
-    s.delete(1);
-    s.add(2);
-    for(let j=3; j<Math.sqrt(n); j++){
-        if(s.has(j)){
-             for(let k=j*2; k<=n; k+=j){    
-                s.delete(k);
-             }
+function solution(n){
+    let arr = Array(n + 1).fill(true).fill(false, 0, 2);
+    for(let i = 2 ; i * i <= n; i++){
+        if(arr[i]){
+            for(let j = i * i; j <= n; j+=i){
+                arr[j] = false;
+            }
         }
     }
-    return s.size;
+    return arr.filter(e => e).length;
 }
