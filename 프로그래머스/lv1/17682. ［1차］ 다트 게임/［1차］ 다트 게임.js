945fe19b8b4b9df380799996651ec10c1bb2e134
Seungmin Lim <92246102+forest-6@users.noreply.github.com>
2023-01-06 13:27:33 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,23 @@
+function solution(dartResult) {
+  let score = [];
+  let arr = dartResult.split('');
+  
+  arr.forEach((v,i)=>{
+    if(Number(v + arr[i+1])) arr.splice(i,2,v + arr[i+1])
+  });
+  
+  arr.forEach((v,i)=>{
+    if(arr[i+1]=='S') score.push(v**1)
+    if(arr[i+1]=='D') score.push(v**2)
+    if(arr[i+1]=='T') score.push(v**3)
+    if(arr[i+2]=='*'){
+      score[score.length-1] = score[score.length-1]*2
+      score[score.length-2] = score[score.length-2]*2
+    }
+    if(arr[i+2]=='#'){
+      score[score.length-1] = score[score.length-1] * -1
+    }
+  });
+  
+  return score.reduce((acc,cur)=>acc+Number(cur))
+}
