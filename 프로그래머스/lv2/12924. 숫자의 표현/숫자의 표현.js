b29003dea5891b5454daa88faf71b0cff9c980e2
Seungmin Lim <92246102+forest-6@users.noreply.github.com>
2023-02-08 21:13:57 +0900

    [level 2] Title: 숫자의 표현, Time: 2.38 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,9 +1,9 @@
 function solution(n) {
-  let answer = n>1 ? 1 : 0;
+  let answer = 1;
   
-  for(let i = 0; i < Math.ceil(n/2); i++){
+  for(let i = 1; i < Math.ceil(n/2); i++){
     let sum = 0;
-    for(let j = i+1; j <= Math.ceil(n/2); j++){
+    for(let j = i; j <= Math.ceil(n/2); j++){
       sum += j;
       if(sum === n) {
         answer ++;        
