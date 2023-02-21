d9a68ce348601afda3f7b083c32aa52699360b9a
Seungmin Lim <92246102+forest-6@users.noreply.github.com>
2023-02-22 01:06:23 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.js
+++ 프로그래머스/lv2/42747. H－Index/H－Index.js
@@ -1,11 +1,6 @@
 function solution(citations) {
-  citations.sort((a,b)=>b-a);
-  for(let i=citations[0]; i>0; i--){
-    let cnt = 0;
-    for(let j=0; j<citations.length; j++){
-      if(i<=citations[j]) cnt++;
-      if(i<=cnt) return i;
-    }
-  }
-  return citations[0];
-} 
+  citations.sort((a, b) => b - a);
+  let i = 0;
+  while(i < citations[i]) i++;
+  return i;
+}
