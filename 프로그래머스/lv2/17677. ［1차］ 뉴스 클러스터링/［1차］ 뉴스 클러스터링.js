a43872680e6d72b7ef33e36740d532ab873e423b
Seungmin Lim <92246102+forest-6@users.noreply.github.com>
2023-03-04 15:00:41 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.32 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/lv2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,4 +1,14 @@
 function solution(str1, str2) {
+  
+  function validation(str){
+    let arr = [];
+    str = str.toUpperCase();
+    for(let i=0; i<str.length-1; i++){
+      arr.push(str[i]+str[i+1]);
+    }
+   return arr.filter(v => 'A'<=v[0] && 'Z'>=v[0] && 'A'<=v[1] && 'Z'>=v[1]);
+  };
+  
   let ans = [0,0];
   let valid1 = validation(str1);
   let valid2 = validation(str2);
@@ -8,16 +18,6 @@ function solution(str1, str2) {
       ans[0]++;
     }
   })  
-  if(ans[0]===valid2.length) return 65536;
   ans[1] = valid1.length + valid2.length;
-  return Math.floor(ans[0]/ans[1]*65536);
-};
-
-function validation(str){
-  let arr = [];
-  str = str.toUpperCase();
-  for(let i=0; i<str.length-1; i++){
-    arr.push(str[i]+str[i+1]);
-  }
- return arr.filter(v => 65<=v.charCodeAt(0) && 95>=v.charCodeAt(0) && 65<=v.charCodeAt(1) && 95>=v.charCodeAt(1))
+  return ans[0] === valid2.length  ? 65536 : Math.floor(ans[0]/ans[1]*65536);
 };
