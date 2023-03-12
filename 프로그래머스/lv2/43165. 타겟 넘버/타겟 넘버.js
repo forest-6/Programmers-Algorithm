3a25af38c21c0b3f5f3af5985ad3a109ebfa6407
Seungmin Lim <92246102+forest-6@users.noreply.github.com>
2023-03-12 23:50:31 +0900

    [level 2] Title: 타겟 넘버, Time: 1.19 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.js
@@ -0,0 +1,16 @@
+function solution(numbers, target) {
+    let answer = 0;
+    const dfs = (index, sum) => {
+        if(index < numbers.length) {
+            dfs(index + 1, sum + numbers[index]);
+            dfs(index + 1, sum - numbers[index]);   
+        } else {
+            if(sum === target) {
+                answer++;
+            }
+        }
+    }
+    dfs(0, 0);
+    
+    return answer;
+}
